c278f23f30557e552b0d9ef1b5a5f51edbd9ad4d
Alan García <alan.ga.g281301@gmail.com>
2023-01-24 19:38:36 -0600

    primer comit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer(){
+    return (
+        <React.Fragment>
+            <footer class="footer">
+                <p>Hola</p>
+            </footer>
+        </React.Fragment>
+    )
+}
+
+export default Footer;
