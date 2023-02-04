b49f314dd2bb5cfa9faae33a5493e80af927ac01
Alan García <alan.ga.g281301@gmail.com>
2023-02-04 13:54:52 -0600

    menu con hash

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,15 +5,15 @@ function Header(){
     return(
         <React.Fragment>
             <header className="header">
-                <Link to={'/'}>                    <a>
+                <Link to={'#/'}>                    <a>
                         <div className="logo">
                         </div>
                     </a>
                 </Link>
                 <div className="menu">
-                    <Link to={'reservacion'}><span><a>Reservación</a></span></Link>
-                    <Link to={'menu'}><span><a>Menú</a></span></Link>
-                    <Link to={'contactanos'}><span><a>Contáctactanos</a></span></Link>
+                    <Link to={'#/reservacion'}><span><a>Reservación</a></span></Link>
+                    <Link to={'#/menu'}><span><a>Menú</a></span></Link>
+                    <Link to={'#/contactanos'}><span><a>Contáctactanos</a></span></Link>
                 </div>
             </header>
         </React.Fragment>
