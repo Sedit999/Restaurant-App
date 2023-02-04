6fe54cc648839331adea4f773ebbe09a08c3df25
Alan García <alan.ga.g281301@gmail.com>
2023-02-04 13:51:59 -0600

    hash router

--- src/App.js
+++ src/App.js
@@ -49,11 +49,11 @@ useEffect(() => {
   return (
     <React.Fragment>
       <Routes>
-        <Route exact path='/' element={<Layout/>}>
+        <Route exact path='#/' element={<Layout/>}>
           <Route index element={<Jumbotron array={postDataReservacion}/>}/>
-          <Route path='reservacion' element={<Reservacion array={postDataReservacion}/>}/>
-          <Route path='menu' element={<Menu array={[postDataBebidas, postDataCafeCaliente, postDataCafeFrio, postDataSandwiches]}/>}/>
-          <Route path='contactanos' element={<Contactanos/>}/>
+          <Route path='#/reservacion' element={<Reservacion array={postDataReservacion}/>}/>
+          <Route path='#/menu' element={<Menu array={[postDataBebidas, postDataCafeCaliente, postDataCafeFrio, postDataSandwiches]}/>}/>
+          <Route path='#/contactanos' element={<Contactanos/>}/>
         </Route>
       </Routes>
     </React.Fragment>
